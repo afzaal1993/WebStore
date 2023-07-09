98beb7a13c8df3f78c24ac49c66541cf2a8a12d6
mafaroq <mafaroq@splonline.com.sa>
2023-07-09 16:33:42 +0300

    enhancement

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {currencyCode} from '../../constants';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
+
+currCode = currencyCode;
 
   ngOnInit(): void {}
 }
