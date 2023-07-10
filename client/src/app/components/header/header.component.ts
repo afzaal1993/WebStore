d218abd26dda9b8a98d4e9db3cd068b468aefdc7
mafaroq <mafaroq@splonline.com.sa>
2023-07-10 11:06:57 +0300

    designing home page

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -8,7 +8,8 @@ import {currencyCode} from '../../constants';
 export class HeaderComponent implements OnInit {
   constructor() {}
 
-currCode = currencyCode;
+  currCode = currencyCode;
 
   ngOnInit(): void {}
 }
+ 
